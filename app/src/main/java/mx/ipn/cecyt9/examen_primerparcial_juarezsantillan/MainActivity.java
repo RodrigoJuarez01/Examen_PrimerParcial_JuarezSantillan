39d2d8ef31aea595effb8af3c6e237a733c5ce65
RodrigoJuarez01 <rodrigo108801@gmail.com>
2018-04-12 09:13:30 -0500

    Comit inicial

--- app/src/main/java/mx/ipn/cecyt9/examen_primerparcial_juarezsantillan/MainActivity.java
+++ app/src/main/java/mx/ipn/cecyt9/examen_primerparcial_juarezsantillan/MainActivity.java
@@ -2,12 +2,47 @@ package mx.ipn.cecyt9.examen_primerparcial_juarezsantillan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+        public void validacion(View v){
+
+
+            EditText tw=(EditText) findViewById(R.id.editText);
+            String usuarioobtenido=tw.getText().toString();
+
+            EditText tw2=(EditText) findViewById(R.id.editText2);
+            String passwordobtenido=tw2.getText().toString();
+
+        if(usuarioobtenido.equals("1") && passwordobtenido.equals("2")){
+        Toast toast1=
+            Toast.makeText(getApplicationContext(),
+                    "Usuario correcto", Toast.LENGTH_SHORT);
+
+            toast1.show();
+
+        }
+        else {
+
+            Toast toast2 =
+                    Toast.makeText(getApplicationContext(),
+                            "Usuario incorrecto", Toast.LENGTH_SHORT);
+
+            toast2.show();
+        }
+        }
+
 }
